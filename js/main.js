ab5434613d3420dc8a45ba145d8179e00ef81ccd
Leonardo Caballero <leonardocaballero25@gmail.com>
2022-10-30 15:39:26 -0600

    Changes to be committed: 	modified:   css/style.css 	modified:   index.html 	modified:   js/main.js 	new file:   php/conexion.php 	new file:   php/registro_usuario.php

--- js/main.js
+++ js/main.js
@@ -1,12 +1,33 @@
+//Declare las variables de los botones y el contenedor
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
 
-
+//Añadi el evento de clic para cambiar de panel
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
 
+//Añadi el evento de clic para cambiar de panel
 signUpButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
+});
+
+//Funcion para mostrar el texto del txtPassword
+function mostrarPassword(){
+		var cambio = document.getElementById("TxtPassword");
+		if(cambio.type == "password"){
+			cambio.type = "text";
+			$('.icon').removeClass('fa fa-eye-slash').addClass('fa fa-eye');
+		}else{
+			cambio.type = "password";
+			$('.icon').removeClass('fa fa-eye').addClass('fa fa-eye-slash');
+		}
+	} 
+	
+	$(document).ready(function () {
+	//CheckBox mostrar contraseña
+	$('#ShowPassword').click(function () {
+		$('#Password').attr('type', $(this).is(':checked') ? 'text' : 'password');
+	});
 });
---
+++ php/registro_usuario.php
@@ -0,0 +1,60 @@
+<?php
+
+    include 'conexion.php';
+
+    $usuario_ = $_POST['Usuario_'];
+    $email_ = $_POST['Email_'];
+    $password_ = $_POST['Password_'];
+
+    // Encryptado
+    $password_ = hash('sha512',$password_);
+
+    $query = "INSERT INTO usuarios(usuario,email,passwor)
+                VALUES('$usuario_','$email_','$password_')";
+
+    //Verificar que el correo no se repita en la base de datos
+    $verificar_correo = mysqli_query($conexion,"SELECT * FROM usuarios WHERE email='$email_'");
+
+    if (mysqli_num_rows($verificar_correo) > 0) {
+        echo '
+        <script>
+            alert("Este correo ya esta registrado, intenta con otro diferente");
+            window.location = "../index.html";
+        </script>
+    ';
+    exit();
+    }
+
+    //Verificar que el usuario no se repita en la base de datos
+    $verificar_usuario = mysqli_query($conexion,"SELECT * FROM usuarios WHERE usuario='$usuario_'");
+    if (mysqli_num_rows($verificar_usuario) > 0) {
+        echo '
+            <script>
+                alert("Este usuario ya esta registrado, intenta con otro diferente");
+                window.location = "../index.html";
+            </script>
+        ';
+        exit();
+    }
+
+
+    $ejecutar = mysqli_query($conexion,$query);
+
+    if($ejecutar){
+        echo '
+            <script>
+                alert("Usuario almacenado en la base de datos");
+                window.location = "../index.html";
+            </script>
+        ';
+
+    }else{
+        echo '
+        <script>
+            alert("Intentelo de nuevo usuario no almacenado");
+            window.location = "../index.html";
+        </script>
+    ';
+
+    }
+    mysqli_close($conexion);
